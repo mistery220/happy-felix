407ff3c13575ef5dac1004242d5208167fdd3e3e
mistery220 <basketball.coder.pro@gmail.com>
2024-01-07 04:09:00 -0800

    'fix'

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -368,7 +368,7 @@ export default function SomethingIveBuilt() {
               </div>
               <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
                 <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
-                  + A mysterious art collection of{" "}
+                  A mysterious art collection of{" "}
                   <span className="text-AAsecondary">5555 NFTs</span> on solana
                   that have risen from the dead. Our French name is a nod to our
                   undead status. We are here to build the{" "}
