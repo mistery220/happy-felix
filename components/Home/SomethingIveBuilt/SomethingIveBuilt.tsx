7cc5a0b0bb1fd41e029149866203ff284dc993d1
mistery220 <basketball.coder.pro@gmail.com>
2024-07-21 08:58:52 +0900

    fix: change project order

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -34,6 +34,175 @@ export default function SomethingIveBuilt() {
       </div>
 
       <div className="flex flex-col space-y-8 md:space-y-28">
+        {/* // ?  Project Meta Tune Club */}
+        <div
+          data-aos="fade-up"
+          className="relative md:grid md:grid-cols-12 w-full md:h-96  "
+        >
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center "
+          >
+            <div className="relative rounded w-full h-full col-start-6 col-span-7 ">
+              <a href={"https://www.3nergywhiz.com/"} target="_blank" rel="noreferrer">
+                <div
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+              <Img
+                src={"/img/meta_tune.png"}
+                alt={"Project Screen shot"}
+                className={`w-full rounded h-full `}
+              />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0 md:order-2">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img
+                  src={"/img/meta_tune.png"}
+                  alt={"Project Screen shot"}
+                  className={`w-full h-full`}
+                />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6   
+            col-span-8 flex flex-col items-start  space-y-3 md:order-1"
+            >
+              <div className="flex flex-col space-y-1  z-10">
+                <span className="text-AAsecondary text-base">
+                  Solana HVAC System
+                </span>
+                <Link href={"https://www.3nergywhiz.com/"}>
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Meta Tune Club
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left ">
+                  This is Solana based <span className="text-AAsecondary">HVAC service system</span>.
+                  <br />
+                  I implemented Mint of <span className="text-AAsecondary">Home Profile NFT, Service Appointment NFT, Contractor Profile NFT, Messenger and Invoice NFT</span>.
+                  <br />
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-start"
+              >
+                <span className="pr-4 z-10">✔Next.js</span>
+                <span className="pr-4 z-10">✔Node.js</span>
+                <span className="pr-4 z-10">✔TypeScript</span>
+                <span className="pr-4 z-10">✔Tailwind CSS</span>
+                <span className="pr-4 z-10">✔Solana</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/comet19950902/energy-colletion" />
+                <a href="" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={"https://www.3nergywhiz.com/"} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* // ?  Project  3nergyWhiz Calc*/}
+        <div
+          data-aos="fade-up"
+          className="relative md:grid md:grid-cols-12 w-full md:h-96 "
+        >
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <a
+                href={"https://calculator.3nergywhiz.com/"}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img
+                src={"/img/energy_calc.png"}
+                alt={"Project Screen shot"}
+                className={`w-full rounded h-full `}
+              />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img
+                  src={"/img/energy_calc.png"}
+                  alt={"Project Screen shot"}
+                  className={`w-full h-full `}
+                />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">
+                  Energy Consume Calculator
+                </span>
+                <Link href={"https://calculator.3nergywhiz.com/"}>
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    3nergyWhiz Calculator
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                  I integrated a <span className="text-AAsecondary">Home Energy Calculator</span> to provide homeowners <br />with an estimated cost of their HVAC usage.
+                  <br />
+                  Utilized external APIs such as Wattbuy to retrieve historical energy rate data <br />based on the homeowner&apos;s zip code.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Solana</span>
+                <span className="pr-4 z-10">Wattbuy</span>
+                <span className="pr-4 z-10">React.js</span>
+                <span className="pr-4 z-10">TypeScript</span>
+                <span className="pr-4 z-10">Tailwind CSS</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/comet19950902/energy-collection" />
+                <a href="" target={"_blank"} rel="noreferrer">
+                  <ExternalLink
+                    url={"https://calculator.3nergywhiz.com/"}
+                    router={router}
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
         {/* // ?  Project Shadow */}
         <div
           data-aos="fade-up"
@@ -197,175 +366,6 @@ export default function SomethingIveBuilt() {
                 <a href="" target={"_blank"} rel="noreferrer">
                   <ExternalLink
                     url={"https://pump.m8s.tech/"}
-                    router={router}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* // ?  Project Meta Tune Club */}
-        <div
-          data-aos="fade-up"
-          className="relative md:grid md:grid-cols-12 w-full md:h-96  "
-        >
-          {/* Left image */}
-          <div
-            className="hidden bg-AAprimary z-10  py-4 
-          absolute md:grid grid-cols-12 w-full h-full  content-center "
-          >
-            <div className="relative rounded w-full h-full col-start-6 col-span-7 ">
-              <a href={"https://www.3nergywhiz.com/"} target="_blank" rel="noreferrer">
-                <div
-                  className="absolute w-full h-full rounded bg-AAprimary 
-           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
-                ></div>
-              </a>
-              <Img
-                src={"/img/meta_tune.png"}
-                alt={"Project Screen shot"}
-                className={`w-full rounded h-full `}
-              />
-            </div>
-          </div>
-
-          {/* right Content */}
-          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
-            {/* background for text in mobile responsive */}
-            <div className="absolute w-full h-full bg-opacity-70 z-0 md:order-2">
-              <div className="relative w-full h-full">
-                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
-                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
-                <Img
-                  src={"/img/meta_tune.png"}
-                  alt={"Project Screen shot"}
-                  className={`w-full h-full`}
-                />
-              </div>
-            </div>
-
-            <div
-              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6   
-            col-span-8 flex flex-col items-start  space-y-3 md:order-1"
-            >
-              <div className="flex flex-col space-y-1  z-10">
-                <span className="text-AAsecondary text-base">
-                  Solana HVAC System
-                </span>
-                <Link href={"https://www.3nergywhiz.com/"}>
-                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
-                    Meta Tune Club
-                  </span>
-                </Link>
-              </div>
-              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
-                <p className="text-gray-300 md:text-gray-400 text-left ">
-                  This is Solana based <span className="text-AAsecondary">HVAC service system</span>.
-                  <br />
-                  I implemented Mint of <span className="text-AAsecondary">Home Profile NFT, Service Appointment NFT, Contractor Profile NFT, Messenger and Invoice NFT</span>.
-                  <br />
-                </p>
-              </div>
-              <ul
-                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
-               text-sm font-Text2 md:justify-start"
-              >
-                <span className="pr-4 z-10">✔Next.js</span>
-                <span className="pr-4 z-10">✔Node.js</span>
-                <span className="pr-4 z-10">✔TypeScript</span>
-                <span className="pr-4 z-10">✔Tailwind CSS</span>
-                <span className="pr-4 z-10">✔Solana</span>
-              </ul>
-              <div className="z-10 flex fle-row space-x-5 ">
-                <GithubIcon link="https://github.com/comet19950902/energy-colletion" />
-                <a href="" target={"_blank"} rel="noreferrer">
-                  <ExternalLink url={"https://www.3nergywhiz.com/"} router={router} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* // ?  Project  3nergyWhiz Calc*/}
-        <div
-          data-aos="fade-up"
-          className="relative md:grid md:grid-cols-12 w-full md:h-96 "
-        >
-          {/* Left image */}
-          <div
-            className="hidden bg-AAprimary z-10  py-4 
-          absolute md:grid grid-cols-12 w-full h-full  content-center"
-          >
-            <div className="relative rounded w-full h-full col-span-7 ">
-              <a
-                href={"https://calculator.3nergywhiz.com/"}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div
-                  className="absolute w-full h-full rounded bg-AAprimary 
-           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
-                ></div>
-              </a>
-
-              <Img
-                src={"/img/energy_calc.png"}
-                alt={"Project Screen shot"}
-                className={`w-full rounded h-full `}
-              />
-            </div>
-          </div>
-
-          {/* right Content */}
-          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
-            {/* background for text in mobile responsive */}
-            <div className="absolute w-full h-full bg-opacity-70 z-0">
-              <div className="relative w-full h-full">
-                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
-                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
-                <Img
-                  src={"/img/energy_calc.png"}
-                  alt={"Project Screen shot"}
-                  className={`w-full h-full `}
-                />
-              </div>
-            </div>
-
-            <div
-              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
-            col-span-8 flex flex-col items-start md:items-end space-y-3"
-            >
-              <div className="flex flex-col space-y-1 md:items-end z-10">
-                <span className="text-AAsecondary text-base">
-                  Energy Consume Calculator
-                </span>
-                <Link href={"https://calculator.3nergywhiz.com/"}>
-                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
-                    3nergyWhiz Calculator
-                  </span>
-                </Link>
-              </div>
-              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
-                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
-                  I integrated a <span className="text-AAsecondary">Home Energy Calculator</span> to provide homeowners <br />with an estimated cost of their HVAC usage.
-                  <br />
-                  Utilized external APIs such as Wattbuy to retrieve historical energy rate data <br />based on the homeowner&apos;s zip code.
-                </p>
-              </div>
-              <ul
-                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
-               text-sm font-Text2 md:justify-end"
-              >
-                <span className="pr-4 z-10">Solana</span>
-                <span className="pr-4 z-10">Wattbuy</span>
-                <span className="pr-4 z-10">React.js</span>
-                <span className="pr-4 z-10">TypeScript</span>
-                <span className="pr-4 z-10">Tailwind CSS</span>
-              </ul>
-              <div className="z-10 flex fle-row space-x-5 ">
-                <GithubIcon link="https://github.com/comet19950902/energy-collection" />
-                <a href="" target={"_blank"} rel="noreferrer">
-                  <ExternalLink
-                    url={"https://calculator.3nergywhiz.com/"}
                     router={router}
                   />
                 </a>
